135d07ce199a691120dd289891fd650b8f8a6524
Martin Kruger <martin@martinkruger.com>
2022-08-11 13:40:53 +0200

    Added basic exercises

---
+++ exercise-1.js
@@ -0,0 +1,9 @@
+// Destructure the properties from the 'car'
+// object below and then log all of them
+
+const car = {
+  manufacturer: 'Nissan',
+  model: 'Skyline GT-R',
+  country: 'Japan',
+  coupe: true,
+};
---
+++ exercise-2.js
@@ -0,0 +1,7 @@
+// Destructure the first, second and fifth value from
+// the array. You are free to call the variables whatever
+// you would like. Once you have destructured the values,
+// log the variables so you know that you have destructured
+// them correctly.
+
+const values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
